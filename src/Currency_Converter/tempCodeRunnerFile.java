1242fe67c9a6a602049ba51b7c41e388ae7c7833
Ferhan Ansari <95950833+mr-farru-20@users.noreply.github.com>
2023-10-25 20:08:19 +0530

    Add files via upload

---
+++ src/Currency_Converter/tempCodeRunnerFile.java
@@ -0,0 +1,7 @@
+if(firstCountry.getSelectedIndex()==0 || secondCountry.getSelectedIndex()==0 || t1.getText().equals(""))
+    //    {
+    //     JOptionPane.showMessageDialog(null,"You Must Select both Countries and must input the amount.","ErrorMessage",
+    //     JOptionPane.INFORMATION_MESSAGE
+    //     );
+    //     return;
+    //   }
